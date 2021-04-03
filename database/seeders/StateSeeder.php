42f09b7d974f4e47fc5be208fbbe76dfbaa9464f
qualwebs <QWGitBit>
2021-04-03 20:03:24 +0530

    state seeder

---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,433 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Common\State;
+use Illuminate\Database\Seeder;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        State::truncate();
+
+        State::insert( [
+            'id'=>1,
+            'state_code'=>'AL',
+            'state_name'=>'Alabama',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>2,
+            'state_code'=>'AK',
+            'state_name'=>'Alaska',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>3,
+            'state_code'=>'AZ',
+            'state_name'=>'Arizona',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>4,
+            'state_code'=>'AR',
+            'state_name'=>'Arkansas',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>5,
+            'state_code'=>'CA',
+            'state_name'=>'California',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>6,
+            'state_code'=>'CO',
+            'state_name'=>'Colorado',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>7,
+            'state_code'=>'CT',
+            'state_name'=>'Connecticut',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>8,
+            'state_code'=>'DE',
+            'state_name'=>'Delaware',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>9,
+            'state_code'=>'DC',
+            'state_name'=>'District of Columbia',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>10,
+            'state_code'=>'FL',
+            'state_name'=>'Florida',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>11,
+            'state_code'=>'GA',
+            'state_name'=>'Georgia',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>12,
+            'state_code'=>'HI',
+            'state_name'=>'Hawaii',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>13,
+            'state_code'=>'ID',
+            'state_name'=>'Idaho',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>14,
+            'state_code'=>'IL',
+            'state_name'=>'Illinois',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>15,
+            'state_code'=>'IN',
+            'state_name'=>'Indiana',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>16,
+            'state_code'=>'IA',
+            'state_name'=>'Iowa',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>17,
+            'state_code'=>'KS',
+            'state_name'=>'Kansas',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>18,
+            'state_code'=>'KY',
+            'state_name'=>'Kentucky',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>19,
+            'state_code'=>'LA',
+            'state_name'=>'Louisiana',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>20,
+            'state_code'=>'ME',
+            'state_name'=>'Maine',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>21,
+            'state_code'=>'MD',
+            'state_name'=>'Maryland',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>22,
+            'state_code'=>'MA',
+            'state_name'=>'Massachusetts',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>23,
+            'state_code'=>'MI',
+            'state_name'=>'Michigan',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>24,
+            'state_code'=>'MN',
+            'state_name'=>'Minnesota',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>25,
+            'state_code'=>'MS',
+            'state_name'=>'Mississippi',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>26,
+            'state_code'=>'MO',
+            'state_name'=>'Missouri',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>27,
+            'state_code'=>'MT',
+            'state_name'=>'Montana',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>28,
+            'state_code'=>'NE',
+            'state_name'=>'Nebraska',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>29,
+            'state_code'=>'NV',
+            'state_name'=>'Nevada',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>30,
+            'state_code'=>'NH',
+            'state_name'=>'New Hampshire',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>31,
+            'state_code'=>'NJ',
+            'state_name'=>'New Jersey',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>32,
+            'state_code'=>'NM',
+            'state_name'=>'New Mexico',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>33,
+            'state_code'=>'NY',
+            'state_name'=>'New York',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>34,
+            'state_code'=>'NC',
+            'state_name'=>'North Carolina',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>35,
+            'state_code'=>'ND',
+            'state_name'=>'North Dakota',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>36,
+            'state_code'=>'OH',
+            'state_name'=>'Ohio',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>37,
+            'state_code'=>'OK',
+            'state_name'=>'Oklahoma',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>38,
+            'state_code'=>'OR',
+            'state_name'=>'Oregon',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>39,
+            'state_code'=>'PA',
+            'state_name'=>'Pennsylvania',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>40,
+            'state_code'=>'PR',
+            'state_name'=>'Puerto Rico',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>41,
+            'state_code'=>'RI',
+            'state_name'=>'Rhode Island',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>42,
+            'state_code'=>'SC',
+            'state_name'=>'South Carolina',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>43,
+            'state_code'=>'SD',
+            'state_name'=>'South Dakota',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>44,
+            'state_code'=>'TN',
+            'state_name'=>'Tennessee',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>45,
+            'state_code'=>'TX',
+            'state_name'=>'Texas',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>46,
+            'state_code'=>'UT',
+            'state_name'=>'Utah',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>47,
+            'state_code'=>'VT',
+            'state_name'=>'Vermont',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>48,
+            'state_code'=>'VA',
+            'state_name'=>'Virginia',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>49,
+            'state_code'=>'WA',
+            'state_name'=>'Washington',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>50,
+            'state_code'=>'WV',
+            'state_name'=>'West Virginia',
+
+
+        ] );
+
+        State::insert( [
+            'id'=>51,
+            'state_code'=>'WI',
+            'state_name'=>'Wisconsin',
+
+        ] );
+
+        State::insert( [
+            'id'=>52,
+            'state_code'=>'WY',
+            'state_name'=>'Wyoming',
+
+        ] );
+    }
+}
