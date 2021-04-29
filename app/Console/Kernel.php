93b24780f31e3a5b0827d9f12870156efa7cab76
qualwebs <QWGitBit>
2021-04-29 15:52:02 +0530

    review points

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CloseApplication;
+use App\Console\Commands\FilterBids;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(CloseApplication::class)->everyMinute();
+        $schedule->command(FilterBids::class)->everyMinute();
     }
 
     /**
