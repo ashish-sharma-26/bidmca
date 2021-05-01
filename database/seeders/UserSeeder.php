e2fb9c447f9ecb8179b191d54fa6d74545ab2667
qualwebs <QWGitBit>
2021-05-01 15:05:25 +0530

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,40 +16,90 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // BORROWER ACCOUNTS
         DB::table('users')->insert([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(10),
-            'email' => 'lender2@mailinator.com',
-            'password' => Hash::make('12345678'),
+            'first_name' => 'Test',
+            'last_name' => 'Merchant1',
+            'email' => 'testmerch1@bidmca.com',
+            'password' => Hash::make('pw123'),
             'phone' => rand(1,1000000000),
-            'user_type' => 2,
+            'user_type' => 3,
             'is_active' => 1,
         ]);
         DB::table('users')->insert([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(10),
-            'email' => 'lender3@mailinator.com',
-            'password' => Hash::make('12345678'),
+            'first_name' => 'Test',
+            'last_name' => 'Merchant2',
+            'email' => 'testmerch2@bidmca.com',
+            'password' => Hash::make('pw123'),
             'phone' => rand(1,1000000000),
-            'user_type' => 2,
+            'user_type' => 3,
             'is_active' => 1,
         ]);
         DB::table('users')->insert([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(10),
-            'email' => 'lender4@mailinator.com',
-            'password' => Hash::make('12345678'),
+            'first_name' => 'Test',
+            'last_name' => 'Merchant3',
+            'email' => 'testmerch3@bidmca.com',
+            'password' => Hash::make('pw123'),
             'phone' => rand(1,1000000000),
-            'user_type' => 2,
+            'user_type' => 3,
             'is_active' => 1,
         ]);
+
+        // BROKER ACCOUNTS
         DB::table('users')->insert([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(10),
-            'email' => 'broker@mailinator.com',
-            'password' => Hash::make('12345678'),
+            'first_name' => 'Test',
+            'last_name' => 'ISO1',
+            'email' => 'testiso1@bidmca.com',
+            'password' => Hash::make('pw123'),
             'phone' => rand(1,1000000000),
             'user_type' => 1,
+            'is_active' => 1,
+        ]);
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'ISO2',
+            'email' => 'testiso2@bidmca.com',
+            'password' => Hash::make('pw123'),
+            'phone' => rand(1,1000000000),
+            'user_type' => 1,
+            'is_active' => 1,
+        ]);
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'ISO3',
+            'email' => 'testiso3@bidmca.com',
+            'password' => Hash::make('pw123'),
+            'phone' => rand(1,1000000000),
+            'user_type' => 1,
+            'is_active' => 1,
+        ]);
+
+        // LENDER ACCOUNTS
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'Synd1',
+            'email' => 'testsynd1@bidmca.com',
+            'password' => Hash::make('pw123'),
+            'phone' => rand(1,1000000000),
+            'user_type' => 2,
+            'is_active' => 1,
+        ]);
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'Synd2',
+            'email' => 'testsynd2@bidmca.com',
+            'password' => Hash::make('pw123'),
+            'phone' => rand(1,1000000000),
+            'user_type' => 2,
+            'is_active' => 1,
+        ]);
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'Synd3',
+            'email' => 'testsynd3@bidmca.com',
+            'password' => Hash::make('pw123'),
+            'phone' => rand(1,1000000000),
+            'user_type' => 2,
             'is_active' => 1,
         ]);
     }
